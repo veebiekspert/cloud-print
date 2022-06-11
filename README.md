01d1d2b652f0744f849aa775255ebf098c136391
veebiekspert <info@veebiekspert.ee>
2022-06-11 16:10:03 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# cloud-print
+# E-liides HTTP Cloud Printer
 Print PDF files to local windows computer over HTTP requests
+
+## Register licence
+Register account and create new 10 day trial licence on site:
+https://www.e-liides.ee/et/printer
+
+## Download .exe file and setup windows program
+
+After receiving licence key download .exe setup to windows computer what is connected to printer
+
+Now insert licence key to opened popup and follow next steps.
+
+## Connect your web application with API
+### Install via composer
+    composer require veebiekspert/cloud-print
+    
+### Use API from your PHP application
+
+    <?php
+    use Veebiekspert\CloudPrint\Api;
+    
+    $licenceKey = ''; // replace this with your e-liides licence key
+    
+    $pdfPath = ''; // path where pdf file exists
+    $pdfFileSource = file_get_contents($pdfPath);
+    
+    $cloudPrinter = new Api($licenceKey);
+    
+    $printers = [];
+    foreach ($cloudPrinter->getPrinters() as $printer) {
+        $printers[] = [
+            'printer_id' => $printer['printer_id'],
+            'name' => $printer['name'],
+        ];
+    }
+    
+    // Get first printer if exists
+    if (!empty($printers)) {
+        $printer = array_shift($printers);
+        $cloudPrinter->addJob($pdfFileSource, $printer['printer_id']);
+        
+        // up to 10 seconds and windows device starts printing
+    }
+    
+# Important notes for success printing
+1. Windows machine must be running
+2. Installed .exe application must be running, default it launch automatically after restart
+    
---
+++ api.php
@@ -0,0 +1,77 @@
+<?php
+namespace Veebiekspert\CloudPrint;
+
+use GuzzleHttp\Client;
+
+/**
+ * E-liides cloud printer API client
+ */
+class Api
+{
+
+    /**
+     * Api key
+     *
+     * @var string
+     */
+    protected $apiKey = '';
+
+    /**
+     * Api url
+     *
+     * @var string
+     */
+    protected $url = 'https://www.e-liides.ee/api/';
+
+    /**
+     * Prinxit constructor.
+     * @param $apiKey
+     */
+    public function __construct($apiKey) {
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * Get List of Printers
+     *
+     * @return array
+     */
+    public function getPrinters() {
+        return $this->sendRequest('cloud-print', 'get-printers');
+    }
+
+    /**
+     * Add job
+     *
+     * @param $pdfSource
+     * @param $printerId
+     * @return mixed
+     */
+    public function addJob($pdfSource, $printerId)
+    {
+        return $this->sendRequest('cloud-print', 'add-job', array(
+            'file' => $pdfSource,
+            'printer_id' => $printerId,
+        ));
+    }
+
+    /**
+     * Send request to Cloud Printer API
+     *
+     * @param string $type
+     * @param string $method
+     * @param array $filter
+     * @return mixed
+     */
+    protected function sendRequest($type, $method, $filter = array())
+    {
+        $filter['api_key'] = $this->apiKey;
+
+        $client = new Client();
+        $response = $client->request('POST', $this->url . $type . '/' . $method, [
+            'verify' => false,
+            'body' => $filter,
+        ]);
+        return json_decode($response->getBody(), true);
+    }
+}
